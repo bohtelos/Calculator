a8fc8062b29770282175194a6565e0c8f50cd874
bohtelos <bohtelos@gmail.com>
2026-04-20 17:21:50 +0300

    Input validation

--- Calculator.c
+++ Calculator.c
@@ -5,42 +5,58 @@ int main() {
     int a, b;
     int result;
 
-    printf("Before everything, please enter two numbers: ");
+    int valid = 0;
+
+    while (valid == 0) {
+
+    printf("Before everything, please enter two numbers: \n");
     scanf(" %d %d", &a, &b);
 
-    printf("Please enter an operator: ");
+    printf("Please enter an operator: \n");
     scanf(" %c", &op);
 
     switch(op) {
 
     case '+':
     result = a + b;
-    printf("The result is: %d", result);
+    printf("The result is: %d\n", result);
+    valid = 1;
     break;
 
     case '-':
     result = a - b;
-    printf("The result is: %d", result);
+    printf("The result is: %d\n", result);
+    valid = 1;
     break;
 
     case '*':
     result = a * b;
-    printf("The result is: %d", result);
+    printf("The result is: %d\n", result);
+    valid = 1;
     break;
 
     case '/':
     if (b != 0) {
 
     result = a / b;
-    printf("The result is: %d", result);
+    printf("The result is: %d\n", result);
+    valid = 1;
     }
 
     else
-    printf("Sorry, cannot divide by 0.");
+    printf("Sorry, cannot divide by 0.\n");
     break;
 
     default:
-    printf("Sorry, I did not recognize that character.");
+    printf("Sorry, I did not recognize that character.\n");
+
+    }
+
+    if (valid == 0) {
+
+    printf("Please try again.\n\n");
+
+    }
 
     }
 
